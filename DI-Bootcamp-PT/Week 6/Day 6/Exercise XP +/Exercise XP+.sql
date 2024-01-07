9488a3014283622fe7dec5d39c2ddc59c398858d
Raphael <wwwrbfsd@gmail.com>
2024-01-07 21:04:44 +0200

    ..

---
+++ DI-Bootcamp-PT/Week 6/Day 6/Exercise XP +/Exercise XP+.sql
@@ -0,0 +1,10 @@
+SELECT * FROM students;
+SELECT first_name, last_name FROM students;
+SELECT * FROM students WHERE id = 2;
+SELECT * FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
+SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a%';
+SELECT first_name, last_name FROM students WHERE first_name ILIKE '%a';
+SELECT first_name, last_name FROM students WHERE SUBSTRING(first_name, LENGTH(first_name)-1, 1) = 'a';
+SELECT first_name, last_name FROM students WHERE id IN (1, 3);
+SELECT * FROM students WHERE birth_date >= '2000-01-01';
---
+++ DI-Bootcamp-PT/Week 6/Day 6/Exercise XP/Exercise XP.sql
@@ -0,0 +1,4 @@
+SELECT * FROM customers WHERE last_name = 'Smith';
+SELECT * FROM customers WHERE last_name = 'Jones';
+
+SELECT * FROM customers WHERE first_name != 'Scott';
