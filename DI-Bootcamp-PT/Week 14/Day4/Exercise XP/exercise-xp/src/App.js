1284a75d7a77bb3551f78bff4f9bcdec0e130d97
Raphael <wwwrbfsd@gmail.com>
2024-03-03 22:24:55 +0200

    .

--- DI-Bootcamp-PT/Week 14/Day4/Exercise XP/exercise-xp/src/App.js
+++ DI-Bootcamp-PT/Week 14/Day4/Exercise XP/exercise-xp/src/App.js
@@ -2,22 +2,26 @@
 import React, { Component } from "react";
 import BuggyCounter from "./components/BuggyCounter";
 import ErrorBoundary from "./components/ErrorBoundary";
+import { LifecycleExample, Child } from "./components/LifeCycleEx";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ErrorBoundary>
-          <BuggyCounter />
-          <BuggyCounter />
-        </ErrorBoundary>
+      (
+        <div>
+          <ErrorBoundary>
+            <BuggyCounter />
+            <BuggyCounter />
+          </ErrorBoundary>
 
-        <ErrorBoundary>
-          <BuggyCounter />
-        </ErrorBoundary>
+          <ErrorBoundary>
+            <BuggyCounter />
+          </ErrorBoundary>
 
-        <BuggyCounter />
-      </div>
+          <BuggyCounter />
+        </div>
+      ),
+      (<LifecycleExample />)
     );
   }
 }
--- DI-Bootcamp-PT/Week 14/Day4/Exercise XP/exercise-xp/src/components/LifeCycleEx.js
+++ DI-Bootcamp-PT/Week 14/Day4/Exercise XP/exercise-xp/src/components/LifeCycleEx.js
@@ -1,10 +1,72 @@
+// LifecycleExample.js
 import React, { Component } from "react";
 
-
 class LifecycleExample extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        favoriteColor: 'red',
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      favoriteColor: "red",
+      show: true,
+    };
+  }
+
+  componentDidMount() {
+    // Simulate a state change after mounting
+    setTimeout(() => {
+      this.setState({ favoriteColor: "yellow" });
+    }, 1000);
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate - Returning true");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate - Component has been updated in the DOM");
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate - In getSnapshotBeforeUpdate");
+    console.log("Previous state:", prevState.favoriteColor);
+    return null;
+  }
+
+  handleDelete = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    console.log("render - Rendering the component");
+    return (
+      <div>
+        <div
+          style={{ border: "2px solid grey", padding: "10px", margin: "10px" }}
+        >
+          <h2 style={{ color: this.state.favoriteColor }}>
+            {this.state.favoriteColor}
+          </h2>
+          <button onClick={() => this.setState({ favoriteColor: "blue" })}>
+            Change Color to Blue
+          </button>
+        </div>
+        <div>
+          {this.state.show && <Child />}
+          <button onClick={this.handleDelete}>Delete</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Child extends Component {
+  componentWillUnmount() {
+    alert("Unmounted!");
+  }
+
+  render() {
+    return <h1>Hello World!</h1>;
+  }
+}
+
+export { LifecycleExample, Child };
