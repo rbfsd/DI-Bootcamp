60faed86bc2c8fa2b4c5a202e8f71f1744d26f5b
Raphael <wwwrbfsd@gmail.com>
2024-03-18 19:35:24 +0200

    carousel daily challenge done

---
+++ DI-Bootcamp-PT/Week 13/Day 6 - Intro to React/Dailly Challenge/caroussel-challenge/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { DemoCarousel } from "./Component/DemoCarousel";
+
+function App() {
+  return (
+    <div className="App">
+      <DemoCarousel />
+    </div>
+  );
+}
+
+export default App;
