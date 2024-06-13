d911a25c54ab60c618909d341596570fd85e35ca
Raphael <wwwrbfsd@gmail.com>
2024-06-13 17:25:01 +0300

    week 22 day 5 mini project

---
+++ DI-Bootcamp-PT/Week 22/Day 5/mini project/game.py
@@ -0,0 +1,32 @@
+# game.py
+
+import random
+
+class Game:
+    def get_user_item(self):
+        while True:
+            user_input = input("Select an item (rock/paper/scissors): ").lower()
+            if user_input in ['rock', 'paper', 'scissors']:
+                return user_input
+            else:
+                print("Invalid choice. Please choose 'rock', 'paper', or 'scissors'.")
+
+    def get_computer_item(self):
+        return random.choice(['rock', 'paper', 'scissors'])
+
+    def get_game_result(self, user_item, computer_item):
+        if user_item == computer_item:
+            return 'draw'
+        elif (user_item == 'rock' and computer_item == 'scissors') or \
+             (user_item == 'paper' and computer_item == 'rock') or \
+             (user_item == 'scissors' and computer_item == 'paper'):
+            return 'win'
+        else:
+            return 'loss'
+
+    def play(self):
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        result = self.get_game_result(user_item, computer_item)
+        print(f"You selected {user_item}. The computer selected {computer_item}. You {result}.")
+        return result
